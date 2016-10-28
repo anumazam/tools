fb088e2bdf35dad6f59eee4970e933c2c5cdfca2
Anum Glasgow <anumazam@gmail.com>
2016-10-28 16:06:57 -0700

    updated to output a table

--- seqcalc.py
+++ seqcalc.py
@@ -2,24 +2,37 @@
 
 # this is a tool for calculating DNA volumes for sequencing rxns.
 
+sample = ['sample']
 numrxns = input("How many sequencing rxns do you have? ")
+firstsample = input("What is the first Quintara ID#? ")
+sample.append('AAG' + str(firstsample))
 
-concvector = []
-for i in range(1,numrxns+1,1):
-	print "Rxn #" + str(i)
+finalDNA = ['finalDNA']
+finalH2O = ['finalH2O']
+concvector = ['miniprep']
+
+for i in range(2,numrxns+2,1):
+	sample.append('AAG' + str(firstsample + i - 1))
+	print "Rxn #" + str(i-1)
 	conc = input("What is the concentration in ng/ul? ")
 	concvector.append(conc)
 
-print "\nTo have 80 ng/ul of each plasmid, add: \n"
+print "\nTo have 80 ng/ul of each plasmid: \n"
 
 totaldna = 0
 
-for j in range(1, numrxns+1,1):
-	print "Rxn #" + str(j)
+for j in range(2, numrxns+2,1):
+
 	finalvolDNA = float(800.0/concvector[j-1])
+	finalDNA.append(finalvolDNA)
+	
 	finalvolH2O = 10.0 - finalvolDNA
+	finalH2O.append(finalvolH2O)
 	
-	format(finalvolDNA, '.2f')
-	format(finalvolH2O, '.2f')
+# create sequencing table
+# sample ; QID# ; DNA vol ; ddH2O vol ; primer ; correct?
 	
-	print "%.2f" % finalvolDNA + " ul DNA and " + "%.2f" % finalvolH2O + " ul ddH2O. \n"
+
+from tabulate import tabulate
+print tabulate ([ sample, concvector, finalDNA, finalH2O ], floatfmt=".2f")
+
