98a18079b88b36ad64c255859f295cb34886f49a
Anum Glasgow <anumazam@gmail.com>
2016-11-28 12:02:35 -0800

    added collect strains function

--- assign_wells.py
+++ assign_wells.py
@@ -23,3 +23,13 @@ from tabulate import tabulate
 print tabulate ([ A, B, C, D, E, F, G, H ], headers = columns, showindex = rowIDs, numalign = "center")
 print "\n"
 
+numstrains = input("How many strains will you distribute on this plate? ")
+numsamples = 96/numstrains
+print "There will be " + str(numsamples) + " samples per strain."
+
+strainsvector=[]
+for n in range(1,numstrains+1,1):
+    strainsvector.append(n)
+
+print strainsvector
+
