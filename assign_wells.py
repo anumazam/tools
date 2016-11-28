f1445733e9bb9021c30b68fed75c60365822ed35
Anum Glasgow <anumazam@gmail.com>
2016-11-28 13:33:25 -0800

    assigns 8 strains to wells on 96 well plate randomly for DHFR assay. +/- MEV split on top/bottom halves of the plate

--- assign_wells.py
+++ assign_wells.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
+import random
 
-# this is a tool for randomly assigning wells for samples on a 96 well plate.
+# this is a tool for randomly assigning wells for 8 strains on a 96 well plate.
 
 # create vectors for rows and columns
 columns = []
@@ -8,28 +9,29 @@ for n in range(1, 13, 1):
     columns.append(n)
 
 rowIDs = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H' ]
-A = ['1', '2', '3', '1', '2', '3', '1', '2', '3', '1', '2', '3', ]
-B = []
-C = []
-D = []
-E = []
-F = []
-G = []
-H = []
+
+# top half of the plate (+mev)
+board = [1]*6 + [2]*6 + [3]*6 + [4]*6 +[5]*6 + [6]*6 +[7]*6 + [8]*6
+random.shuffle(board)
+board = [board[i:i + 12] for i in xrange(0, 48, 12)]
+
+A = board[0]
+B = board[1]
+C = board[2]
+D = board[3]
+
+# bottom half of the plate (-mev)
+board = [1]*6 + [2]*6 + [3]*6 + [4]*6 +[5]*6 + [6]*6 +[7]*6 + [8]*6
+random.shuffle(board)
+board = [board[i:i + 12] for i in xrange(0, 48, 12)]
+
+E = board[0]
+F = board[1]
+G = board[2]
+H = board[3]
 
 # create table for plate
 print "\n"
 from tabulate import tabulate
 print tabulate ([ A, B, C, D, E, F, G, H ], headers = columns, showindex = rowIDs, numalign = "center")
 print "\n"
-
-numstrains = input("How many strains will you distribute on this plate? ")
-numsamples = 96/numstrains
-print "There will be " + str(numsamples) + " samples per strain."
-
-strainsvector=[]
-for n in range(1,numstrains+1,1):
-    strainsvector.append(n)
-
-print strainsvector
-
