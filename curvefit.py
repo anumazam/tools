efaf4f1a5c14d0d0e3598b739353b07175ff0d78
Anum Glasgow <anumazam@gmail.com>
2017-01-21 18:35:40 -0800

    fixed hyperbolic function

--- curvefit.py
+++ curvefit.py
@@ -8,10 +8,9 @@ from matplotlib import pyplot as plt
 
 xdata = [500, 250, 200, 125, 62.5, 31.3, 15.6, 7.8]
 ydata = [1.000000046, 0.823575113, 0.691950578, 0.634824117, 0.488234805, 0.354962446, 0.217080759,0.15088949]
-plt.plot(xdata, ydata, '.')
 
-def funcHyp(x, qi, exp, di):
-    return qi*(1+exp*di*x)**(-1/exp)
+def funcHyp(x, a, b):
+    return (x+a)/(x*b)
 
 trialX = np.linspace(xdata[0], xdata[-1], 1000)
 
