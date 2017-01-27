d2c4ac3def7524c6ef7e807fe4f3f6b940d54292
Anum Glasgow <anumazam@gmail.com>
2017-01-27 10:57:26 -0800

    added ddGFP TXTL data and modified normalization

--- curvefit_TXTL.py
+++ curvefit_TXTL.py
@@ -6,10 +6,14 @@ import numpy as np
 from scipy.optimize import curve_fit
 from matplotlib import pyplot as plt
 
-xdata = [0.5, 1, 2, 5, 10, 20, 50, 100, 200]
-ydata = [203955.00, 237793.33, 247993.33, 243903.33, 260283.33, 263960.00, 264170.00, 268793.33, 262210.00]
-satpt = ydata[-1]
-ydata_norm = [x / satpt for x in ydata]
+xdata = [0.1, 0.5, 1, 2, 5, 10, 20]
+ydata1 = [201196.00, 203955.00, 237793.33, 247993.33, 243903.33, 260283.33, 263960.00]
+ydata2 = [28850.00, 33551.67, 35446.33, 38762.33, 39945.33, 42302.00, 43795.00]
+satpt = ydata1[-1]
+ydata1_norm = [x / satpt for x in ydata1]
+satpt = ydata2[-1]
+ydata2_norm = [y / satpt for y in ydata2]
+
 #error = [0.029748604,0.044241493,0.053897674,0.068093355,0.046003875,0]
 
 #xdata = [0.001, 1, 10, 50, 200, 1000]
@@ -17,22 +21,28 @@ ydata_norm = [x / satpt for x in ydata]
 #error = [1340.16, 1172.84, 774.54, 1221.61, 408.08, 494.14]
 
 def funcHyp(x, a):
-    return x/(a+x)
+    return 1*x/(a+x)
 
-trialX = np.linspace(.5, 1000, 1000)
+trialX = np.linspace(.1, 25, 100)
 
 # fit hyperbolic fxn
-popt, pcov = curve_fit(funcHyp, xdata, ydata_norm)
-yHYP = funcHyp(trialX, *popt)
+popt, pcov = curve_fit(funcHyp, xdata, ydata1_norm)
+yHYP1 = funcHyp(trialX, *popt)
+print popt
+
+popt, pcov = curve_fit(funcHyp, xdata, ydata2_norm)
+yHYP2 = funcHyp(trialX, *popt)
 print popt
 
 # plot
 plt.figure()
 
-plt.plot(xdata, ydata_norm, 'g+', label='data', marker='s', markersize=8)
-plt.plot(trialX, yHYP, 'k-',ls='--', label='fit')
+plt.plot(xdata, ydata1_norm, 'g+', label='data', marker='s', markersize=8)
+plt.plot(xdata, ydata2_norm, 'y+', label='data', marker='s', markersize=8)
+plt.plot(trialX, yHYP1, 'g-',ls='--', label='fit')
+plt.plot(trialX, yHYP2, 'y-',ls='--', label='fit')
 #plt.errorbar(xdata, ydata, yerr=error, linestyle = "none", color = "black")
-plt.xscale('log', nonposy='clip')
+plt.xscale('linear', nonposy='clip')
 #plt.ylim(0.6, 1.05)
 
 plt.legend(loc=4)
