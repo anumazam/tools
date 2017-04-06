b45f78e38a45aaa551abaae3472d75a6eb1d6231
Anum Glasgow <anumazam@gmail.com>
2017-04-05 18:41:50 -0700

    includes #chains

--- change_resfile_ptmutant.py
+++ change_resfile_ptmutant.py
@@ -2,12 +2,20 @@
 
 # this is a script to generate a point mutation resfile for every residue in a structure.
 
-numres = input("How many residues does your structure have? ")
+chainletter_list = ["A", "B", "C", "D", "E"]
 
-for i in range(numres):
-    x = str(i+1)
-    tempfilename = ("res" + x + ".res")
+numchains = input("How many chains does your structure have? ")
+for j in range(numchains):
+    
+    chainletter = chainletter_list[j]
 
-    f = open(tempfilename, "w+")
-    f.write("NATAA \nSTART \n* A NATAA \n" + x + " A ALLAA")
-    f.close
+    numres = input("How many residues does your chain " + chainletter + " have? ")
+
+    for i in range(numres):
+        x = str(i+1)
+        tempfilename = ("res" + x + ".res")
+
+        f = open(tempfilename, "w+")
+        f.write("NATAA \nSTART \n* A NATAA \n" + x + " " + chainletter + " ALLAA")
+        
+        f.close
