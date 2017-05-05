e8bf159213187a04799c8f15036bbff1236f7bc1
Anum Glasgow <anumazam@gmail.com>
2017-05-04 21:15:07 -0700

    extract lowest energy and sequence from coupled moves trials

--- find_lowest_energy.py
+++ find_lowest_energy.py
@@ -17,21 +17,22 @@ for i in range(0, len(allfiles)):
     if allfiles[i].endswith(suffix):
         fasta_files.append(allfiles[i])
 
-path = "/Users/anumglasgow/Dropbox/Research/Structures/transcription_factor/lacI/20170504_CM_LacI_ONPF/loop1/2p9h_ONPF_A_withH_noH2O_0001.fasta"
+for i in range(0, len(fasta_files)):
+    path = "/Users/anumglasgow/Dropbox/Research/Structures/transcription_factor/lacI/20170504_CM_LacI_ONPF/loop1/" + fasta_files[i]
 
-seqIDs = []
-sequences = []
-all_energies=[]
+    seqIDs = []
+    sequences = []
+    all_energies=[]
 
-for seq_record in SeqIO.parse(path, "fasta"):
-    all_energies.append(seq_record.description.split()[1])
-    seqIDs.append(seq_record.id)
-    sequences.append(str(seq_record.seq))
+    for seq_record in SeqIO.parse(path, "fasta"):
+        all_energies.append(seq_record.description.split()[1])
+        seqIDs.append(seq_record.id)
+        sequences.append(str(seq_record.seq))
 
-lowest_energy = np.amin(map(float, all_energies))
+    lowest_energy = np.amin(map(float, all_energies))
 
-for i in [i for i,x in enumerate(map(float, all_energies)) if x == lowest_energy]:
-    position = i
+    for i in [i for i,x in enumerate(map(float, all_energies)) if x == lowest_energy]:
+        position = i
 
-print ">" + seqIDs[position]
-print sequences[position]
+    print ">" + seqIDs[position]
+    print sequences[position]
