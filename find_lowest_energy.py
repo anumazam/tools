23c824275652305be46da9679d883e31af1c5828
Anum Glasgow <anumazam@gmail.com>
2017-05-04 21:28:39 -0700

    extract lowest energy and sequence from coupled moves trials, multiple loops

--- find_lowest_energy.py
+++ find_lowest_energy.py
@@ -6,33 +6,40 @@ from Bio import SeqIO
 from os import listdir
 import numpy as np
 
-path = "/Users/anumglasgow/Dropbox/Research/Structures/transcription_factor/lacI/20170504_CM_LacI_ONPF/loop1/"
+loops = 7
 
-from os.path import isfile, join
-allfiles = [f for f in listdir(path) if isfile(join(path, f))]
+for n in range(1, loops + 1):
+    
+    print "///////////////////////////////////////////////////"
+    print "loop #" + str(n)
 
-fasta_files = []
-suffix = ".fasta"
-for i in range(0, len(allfiles)):
-    if allfiles[i].endswith(suffix):
-        fasta_files.append(allfiles[i])
+    path = "/Users/anumglasgow/Dropbox/Research/Structures/transcription_factor/lacI/20170504_CM_LacI_ONPF/loop" + str(n) + "/"
 
-for i in range(0, len(fasta_files)):
-    path = "/Users/anumglasgow/Dropbox/Research/Structures/transcription_factor/lacI/20170504_CM_LacI_ONPF/loop1/" + fasta_files[i]
+    from os.path import isfile, join
+    allfiles = [f for f in listdir(path) if isfile(join(path, f))]
 
-    seqIDs = []
-    sequences = []
-    all_energies=[]
+    fasta_files = []
+    suffix = ".fasta"
+    for i in range(0, len(allfiles)):
+        if allfiles[i].endswith(suffix):
+            fasta_files.append(allfiles[i])
 
-    for seq_record in SeqIO.parse(path, "fasta"):
-        all_energies.append(seq_record.description.split()[1])
-        seqIDs.append(seq_record.id)
-        sequences.append(str(seq_record.seq))
+    for i in range(0, len(fasta_files)):
+        path = "/Users/anumglasgow/Dropbox/Research/Structures/transcription_factor/lacI/20170504_CM_LacI_ONPF/loop" + str(n) + "/" + fasta_files[i]
 
-    lowest_energy = np.amin(map(float, all_energies))
+        seqIDs = []
+        sequences = []
+        all_energies=[]
 
-    for i in [i for i,x in enumerate(map(float, all_energies)) if x == lowest_energy]:
-        position = i
+        for seq_record in SeqIO.parse(path, "fasta"):
+            all_energies.append(seq_record.description.split()[1])
+            seqIDs.append(seq_record.id)
+            sequences.append(str(seq_record.seq))
 
-    print ">" + seqIDs[position]
-    print sequences[position]
+        lowest_energy = np.amin(map(float, all_energies))
+
+        for i in [i for i,x in enumerate(map(float, all_energies)) if x == lowest_energy]:
+            position = i
+
+            print ">" + seqIDs[position]
+            print sequences[position]
