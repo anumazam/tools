6b2b411df7ff99aaa4b9395591537473fdaac506
Anum Glasgow <anumazam@gmail.com>
2017-05-04 19:11:36 -0700

    extract lowest energy and sequence from coupled moves trials

--- find_lowest_energy.py
+++ find_lowest_energy.py
@@ -3,7 +3,19 @@
 # find the lowest energy sequence in .fasta files from coupled moves output.
 
 from Bio import SeqIO
+from os import listdir
 import numpy as np
+
+path = "/Users/anumglasgow/Dropbox/Research/Structures/transcription_factor/lacI/20170504_CM_LacI_ONPF/loop1/"
+
+from os.path import isfile, join
+allfiles = [f for f in listdir(path) if isfile(join(path, f))]
+
+fasta_files = []
+suffix = ".fasta"
+for i in range(0, len(allfiles)):
+    if allfiles[i].endswith(suffix):
+        fasta_files.append(allfiles[i])
 
 path = "/Users/anumglasgow/Dropbox/Research/Structures/transcription_factor/lacI/20170504_CM_LacI_ONPF/loop1/2p9h_ONPF_A_withH_noH2O_0001.fasta"
 
