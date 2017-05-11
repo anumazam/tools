1cecd2059d51d41f2d823cac2c1aa4175f0b936b
Anum Glasgow <anum@delaware.ucsf.edu>
2017-05-11 10:36:53 -0700

    writes FASTA outputs to separate files

--- find_lowest_energy.py
+++ find_lowest_energy.py
@@ -7,13 +7,14 @@ from os import listdir
 import numpy as np
 
 loops = 7
+folder = "20170510_LacI_IPTG"
 
 for n in range(1, loops + 1):
     
     print "///////////////////////////////////////////////////"
     print "loop #" + str(n)
 
-    path = "/Users/anumglasgow/Dropbox/Research/Structures/transcription_factor/lacI/20170504_CM_LacI_ONPF/loop" + str(n) + "/"
+    path = "/Users/anumglasgow/Dropbox/Research/Structures/transcription_factor/lacI/" + folder + "/loop" + str(n) + "/"
 
     from os.path import isfile, join
     allfiles = [f for f in listdir(path) if isfile(join(path, f))]
@@ -23,9 +24,10 @@ for n in range(1, loops + 1):
     for i in range(0, len(allfiles)):
         if allfiles[i].endswith(suffix):
             fasta_files.append(allfiles[i])
+        filename = "/Users/anumglasgow/Dropbox/Research/Structures/transcription_factor/lacI/" + folder + "/loop" + str(n) + "/sequences_loop" + str(n)
 
     for i in range(0, len(fasta_files)):
-        path = "/Users/anumglasgow/Dropbox/Research/Structures/transcription_factor/lacI/20170504_CM_LacI_ONPF/loop" + str(n) + "/" + fasta_files[i]
+        path = "/Users/anumglasgow/Dropbox/Research/Structures/transcription_factor/lacI/" + folder + "/loop" + str(n) + "/" + fasta_files[i]
 
         seqIDs = []
         sequences = []
@@ -38,8 +40,13 @@ for n in range(1, loops + 1):
 
         lowest_energy = np.amin(map(float, all_energies))
 
+        target = open(filename, 'a')
         for i in [i for i,x in enumerate(map(float, all_energies)) if x == lowest_energy]:
             position = i
 
             print ">" + seqIDs[position]
             print sequences[position]
+
+            target.write(">" + seqIDs[position] + "\n")
+            target.write(sequences[position] + "\n")
+        target.close()
