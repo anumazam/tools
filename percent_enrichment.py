338298233394eee2f0ae66e32414e5dc6a62f53f
anumazam <anumazam@gmail.com>
2017-05-17 15:23:23 -0700

    calculate percent enrichment for each residue for a position in a designed loop

--- percent_enrichment.py
+++ percent_enrichment.py
@@ -3,6 +3,7 @@
 # calculate percent enrichment for each residue for a position in a designed loop
 
 from Bio import SeqIO
+import matplotlib.pyplot as plt
 
 nativeseq = raw_input("What is the native sequence? ")
 seqfile = raw_input("What is the name of your sequences file? ")
@@ -18,20 +19,29 @@ for seq_record in SeqIO.parse(path, "fasta"):
     seqIDs.append(seq_record.id)
     sequences.append(str(seq_record.seq))
 
-res_dict = {"G":0, "A":0, "L":0, "M":0, "F":0, "W":0, "K":0, "Q":0, "E":0, "S":0, "P":0, "V":0, "I":0, "C":0, "Y":0, "H":0, "R":0, "N":0, "D":0, "T":0}
 
 temp = 0
-residues = []
 
-for n in range(0, len(sequences)):
-    residues.append(sequences[n][0])
+for m in range(0, numres):
+    
+    res_dict = {"G":0, "A":0, "L":0, "M":0, "F":0, "W":0, "K":0, "Q":0, "E":0, "S":0, "P":0, "V":0, "I":0, "C":0, "Y":0, "H":0, "R":0, "N":0, "D":0, "T":0}
+    
+    residues = []
+    for n in range(0, len(sequences)):
+        residues.append(sequences[n][m])
 
-for k, v in res_dict.iteritems():
-    for n in range(1, len(residues) + 1):
-        if k == residues[n - 1]:
-            temp += 1
-    res_dict[k] = temp
-    temp = 0
+        for k, v in res_dict.iteritems():
+            for n in range(1, len(residues) + 1):
+                if k == residues[n - 1]:
+                    temp += 1
+            res_dict[k] = temp
+            temp = 0
 
-for k, v in res_dict.items():
-    print (k, v)
+    plt.bar(range(len(res_dict)), res_dict.values(), align = 'center')
+    plt.xticks(range(len(res_dict)), list(res_dict.keys()))
+    plt.title("Residue #" + str(m+1) + ": " + nativeseq[m])
+    plt.ylabel("Frequency")
+    plt.xlabel("Designed residue")
+
+    plt.show()
+
