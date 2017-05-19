e2d4cbd71fded93e2ac4c3f17ff017bf87e3e814
anumazam <anumazam@gmail.com>
2017-05-19 12:02:14 -0700

    added function to find first shell residues

--- create_first_shell_resfile.py
+++ create_first_shell_resfile.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python
-# generate a resfile for first-shell residues (6 angstroms) around a ligand bound to a protein structure.
+# find first-shell residues (6 angstroms) around a ligand bound to a protein structure and generate a resfile.
 # assumes ligand is the only residue in chain X.
 
 from pymol import cmd,stored
-resfile_path = "/Users/anum/Dropbox/Research/Structures/transcription_factor/lacI/20170501_coupledmoves_LacI_ONPF/"
+resfile_path = "/Users/anumglasgow/Dropbox/Research/Structures/transcription_factor/lacI/20170518_coupledmoves/"
 ligand_name = "NPF"
 
 def first_shell_res( userSelection ):
+    
     stored.FSresidues = []
 
     userSelection = userSelection + " and n. c+o+n+ca"
     cmd.iterate(userSelection, "stored.FSresidues.append( (resi, resn) )")
-    print stored.FSresidues
     
     list_FSresidues = "\n".join(str(x) for x in stored.FSresidues )
     
@@ -21,6 +21,11 @@ def first_shell_res( userSelection ):
     f.write("NATRO \nSTART \n" + list_FSresidues)
     f.close
 
+def first_shell_select():
+
+    shell_name = "near" + ligand_name
+    cmd.select(shell_name, "chain X around 6")
 
 # let pymol know about this function
 cmd.extend("first_shell_res", first_shell_res)
+cmd.extend("first_shell_select", first_shell_select)
